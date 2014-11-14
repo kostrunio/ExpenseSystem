c80c908b4a6bc5103ae240ba92067593edf44c8e
sklad <sklad>
2014-11-14 10:00:43 +0000

    usuniêcie przyk³adowego kodu

--- src/pl/kostro/expensesystem/ExpenseSystemUI.java
+++ src/pl/kostro/expensesystem/ExpenseSystemUI.java
@@ -2,13 +2,12 @@ package pl.kostro.expensesystem;
 
 import javax.servlet.annotation.WebServlet;
 
+import pl.kostro.expensesystem.components.LoginPage;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -27,13 +26,7 @@ public class ExpenseSystemUI extends UI {
 		layout.setMargin(true);
 		setContent(layout);
 
-		Button button = new Button("Click Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
+		layout.addComponent(new LoginPage());
 	}
 
 }
