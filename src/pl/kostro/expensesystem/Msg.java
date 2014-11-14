0bc80e6689f9bf932e5030173eabf39906d75e1d
sklad <sklad>
2014-11-14 10:12:31 +0000

    utworzenie klasy do internationalizacji

---
+++ src/pl/kostro/expensesystem/Msg.java
@@ -0,0 +1,22 @@
+package pl.kostro.expensesystem;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Msg {
+	private static final String BUNDLE_NAME = "pl.kostro.expensesystem.messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private Msg() {
+	}
+
+	public static String getMsg(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
