c4e30319b0eecd173129b9ce6687841b0fdff9db
sklad <sklad>
2014-11-25 08:28:25 +0000

    dodanie creationDate i anotacji modelu

--- src/pl/kostro/expensesystem/model/User.java
+++ src/pl/kostro/expensesystem/model/User.java
@@ -1,28 +1,58 @@
 package pl.kostro.expensesystem.model;
 
+import java.util.Date;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="TYPE", discriminatorType=DiscriminatorType.INTEGER)
 public class User {
-	private int id;
-	private String name;
-	
-	public User(String name) {
-		this.name = name;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String toString() {
-		return getName();
-	}
+  
+  @Id
+  @GeneratedValue(generator = "increment")
+  @GenericGenerator(name = "increment", strategy = "increment")
+  private int id;
+  private String name;
+  private Date creationDate = new Date();
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public Date getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public String toString() {
+    return getName();
+  }
 }
