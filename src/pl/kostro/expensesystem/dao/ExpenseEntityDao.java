03d6b98ce463dc842f2c1a7970139ee453aba292
sklad <sklad>
2014-12-18 06:40:08 +0000

    dodanie metod pobierania wyników przez zapytania

--- src/pl/kostro/expensesystem/dao/ExpenseEntityDao.java
+++ src/pl/kostro/expensesystem/dao/ExpenseEntityDao.java
@@ -1,8 +1,15 @@
 package pl.kostro.expensesystem.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import pl.kostro.expensesystem.model.AbstractEntity;
 
 public class ExpenseEntityDao {
   private static EntityManagerFactory entityManagerFactory;
@@ -30,6 +37,34 @@ public class ExpenseEntityDao {
 
   public static void close() {
     getEntityManager().close();
+  }
+  
+  @SuppressWarnings("unchecked")
+  public static <T extends AbstractEntity> List<T> findByNamedQueryWithParameters(final String queryName, final Map<String, ?> parameters, final Class<T> entityClass) {
+    try {
+      return createNamedQueryWithParameters(queryName, parameters, entityClass).getResultList();
+    } catch (NoResultException e) {
+      return null;
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T extends AbstractEntity> T findSingleByNamedQueryWithParameters(final String queryName, final Map<String, ?> parameters, final Class<T> entityClass) {
+    try {
+      return (T) createNamedQueryWithParameters(queryName, parameters, entityClass).getSingleResult();
+    } catch (NoResultException e) {
+      return null;
+    }
+  }
+  
+  private static <T extends AbstractEntity> Query createNamedQueryWithParameters(final String queryName, final Map<String, ?> parameters, final Class<T> entityClass) {
+    Query namedQuery = getEntityManager().createNamedQuery(queryName, entityClass);
+    if (parameters != null) {
+      for (Map.Entry<String, ?> entry : parameters.entrySet()) {
+        namedQuery.setParameter(entry.getKey(), entry.getValue());
+      }
+    }
+    return namedQuery;
   }
 
 }
