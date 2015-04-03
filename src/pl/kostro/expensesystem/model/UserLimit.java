da961a64cd5fcbc339e415cce20e358b9ab5a652
sklad <sklad>
2015-04-03 06:43:14 +0000

    dodanie orderId i userSummary

--- src/pl/kostro/expensesystem/model/UserLimit.java
+++ src/pl/kostro/expensesystem/model/UserLimit.java
@@ -1,10 +1,17 @@
 package pl.kostro.expensesystem.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -19,22 +26,23 @@ public class UserLimit extends AbstractEntity {
   private int id;
   @OneToOne
   private User user;
-  private int limit;
+  private BigDecimal limit;
+  private int orderId;
   @ManyToOne
   private ExpenseSheet expenseSheet;
+  @OneToMany(mappedBy="userLimit", fetch=FetchType.EAGER)
+  @OrderBy(value="date")
+  private List<UserSummary> userSummaryList;
+  private boolean continuousSummary;
   
   public UserLimit() {
     super();
   }
 
-  public UserLimit(User user, int limit, ExpenseSheet expenseSheet) {
+  public UserLimit(User user, BigDecimal limit, ExpenseSheet expenseSheet) {
     this.user = user;
     this.limit = limit;
     this.expenseSheet = expenseSheet;
-  }
-
-  public User getUser() {
-    return user;
   }
 
   public int getId() {
@@ -44,16 +52,28 @@ public class UserLimit extends AbstractEntity {
     this.id = id;
   }
   
+  public User getUser() {
+    return user;
+  }
+  
   public void setUser(User user) {
     this.user = user;
   }
 
-  public int getLimit() {
+  public BigDecimal getLimit() {
     return limit;
   }
 
-  public void setLimit(int limit) {
+  public void setLimit(BigDecimal limit) {
     this.limit = limit;
+  }
+  
+  public int getOrderId() {
+    return orderId;
+  }
+  
+  public void setOrderId(int orderId) {
+    this.orderId = orderId;
   }
   
   public ExpenseSheet getExpenseSheet() {
@@ -62,6 +82,24 @@ public class UserLimit extends AbstractEntity {
 
   public void setExpenseSheet(ExpenseSheet expenseSheet) {
     this.expenseSheet = expenseSheet;
+  }
+  
+  public List<UserSummary> getUserSummaryList() {
+    if (userSummaryList == null)
+      userSummaryList = new ArrayList<UserSummary>();
+    return userSummaryList;
+  }
+
+  public void setUserSummaryList(List<UserSummary> userSummaryList) {
+    this.userSummaryList = userSummaryList;
+  }
+  
+  public boolean isContinuousSummary() {
+    return continuousSummary;
+  }
+  
+  public void setContinuousSummary(boolean continuousSummary) {
+    this.continuousSummary = continuousSummary;
   }
 
   public String toString() {
