6cc431f45f1bca258e8fc3981bdbf94ce73a0c9b
sklad <sklad>
2015-04-30 09:49:46 +0000

    prezentowanie wydatków z podzia³em na u¿ytkowników

--- src/pl/kostro/expensesystem/utils/Converter.java
+++ src/pl/kostro/expensesystem/utils/Converter.java
@@ -6,20 +6,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import pl.kostro.expensesystem.model.ExpenseSheet;
+import pl.kostro.expensesystem.model.UserLimit;
+
 import com.vaadin.ui.components.calendar.event.BasicEvent;
 import com.vaadin.ui.components.calendar.event.CalendarEvent;
 
 public class Converter {
 	
-	public static List<CalendarEvent> transformExpensesToEvents(Map<Date, DateExpense> dateExpenseMap) {
+	public static List<CalendarEvent> transformExpensesToEvents(ExpenseSheet expenseSheet, Map<Date, DateExpense> dateExpenseMap) {
 		Set<Date> dateSet = dateExpenseMap.keySet();
 		List<CalendarEvent> calendarEvents = new ArrayList<CalendarEvent>();
 		BasicEvent event;
 		for (Date date : dateSet) {
 			DateExpense dateExpense = dateExpenseMap.get(date);
-			event = new BasicEvent(dateExpense.getSumString(), null, date);
-			event.setAllDay(true);
-			calendarEvents.add(event);
+			
+			//userLimit events
+			for (UserLimit userLimit : expenseSheet.getUserLimitListDesc()) {
+				UserLimitExpense userLimitExpenseMap = dateExpense.getUserLimitExpenseMap().get(userLimit);
+				if (userLimitExpenseMap == null)
+					continue;
+				event = new BasicEvent(userLimitExpenseMap.getSumString(), null, date);
+				event.setAllDay(true);
+				event.setStyleName(""+userLimitExpenseMap.getUserLimit().getOrder());
+				calendarEvents.add(event);
+			}
 		}
 		return calendarEvents;
 	}
