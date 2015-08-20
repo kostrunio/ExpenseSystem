6eb1ddc4db288a7058f996dba55bee6494e4f6ef
sklad <sklad>
2015-08-20 21:51:48 +0000

    klasa od notyfikacji

---
+++ src/pl/kostro/expensesystem/notification/ShowNotification.java
@@ -0,0 +1,42 @@
+package pl.kostro.expensesystem.notification;
+
+import java.math.BigDecimal;
+
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.themes.ValoTheme;
+
+public class ShowNotification {
+
+  public static void logonProblem() {
+    Notification notification = new Notification("B³¹d logowania");
+    notification.setDescription("B³êdna nazwa u¿ytkownika lub has³o");
+    notification.setStyleName(ValoTheme.NOTIFICATION_ERROR + " " + ValoTheme.NOTIFICATION_SMALL + " " + ValoTheme.NOTIFICATION_CLOSABLE);
+    notification.setPosition(Position.BOTTOM_CENTER);
+    notification.setDelayMsec(10000);
+    notification.show(Page.getCurrent());
+  }
+  
+  public static void dbProblem() {
+    Notification notification = new Notification("Problem z baza danych");
+    notification.setDescription("Nie udalo sie polaczyc do bazy danych");
+    notification.setStyleName(ValoTheme.NOTIFICATION_ERROR + " " + ValoTheme.NOTIFICATION_SMALL + " " + ValoTheme.NOTIFICATION_CLOSABLE);
+    notification.setPosition(Position.BOTTOM_CENTER);
+    notification.setDelayMsec(10000);
+    notification.show(Page.getCurrent());
+  }
+  
+  public static void changeSummary(String userName, BigDecimal actSum, BigDecimal newSum) {
+    Notification notification = new Notification("Suma wydatków");
+    StringBuilder desc = new StringBuilder();
+    desc.append("Zaktualizowano wydatki dla: " + userName + "\n");
+    desc.append("Bylo: " + actSum + ", jest: " + newSum);
+    notification.setDescription(desc.toString());
+    notification.setStyleName(ValoTheme.NOTIFICATION_WARNING + " " + ValoTheme.NOTIFICATION_SMALL + " " + ValoTheme.NOTIFICATION_CLOSABLE);
+    notification.setPosition(Position.BOTTOM_CENTER);
+    notification.setDelayMsec(5000);
+    notification.show(Page.getCurrent());
+  }
+
+}
