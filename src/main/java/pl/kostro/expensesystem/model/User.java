4d88b904648fa283407960418cc992443430e9ba
Michal Kostro <miketo@o2.pl>
2015-10-30 17:22:33 +0100

    usunięcie importu

--- src/main/java/pl/kostro/expensesystem/model/User.java
+++ src/main/java/pl/kostro/expensesystem/model/User.java
@@ -8,7 +8,6 @@ import javax.persistence.DiscriminatorType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
