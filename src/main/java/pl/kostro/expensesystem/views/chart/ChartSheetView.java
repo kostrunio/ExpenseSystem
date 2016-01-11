74c2e3e21dc8463afac457a7210eabcd53829090
Michał Kostro <michal.kostro@cgi.com>
2016-01-11 02:21:38 +0100

    dodanie fitrowania na wykresach

--- src/main/java/pl/kostro/expensesystem/views/chart/ChartSheetView.java
+++ src/main/java/pl/kostro/expensesystem/views/chart/ChartSheetView.java
@@ -2,7 +2,9 @@ package pl.kostro.expensesystem.views.chart;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import pl.kostro.expensesystem.Msg;
 import pl.kostro.expensesystem.model.Category;
@@ -26,10 +28,10 @@ import at.downdrown.vaadinaddons.highchartsapi.model.series.ColumnChartSeries;
 import at.downdrown.vaadinaddons.highchartsapi.model.series.LineChartSeries;
 
 import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.OptionGroup;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
@@ -38,10 +40,10 @@ import com.vaadin.ui.Button.ClickEvent;
 public class ChartSheetView extends CustomComponent {
   private static final long serialVersionUID = -7074536397686956050L;
 
-  private VerticalLayout mainLayout;
-  
-  private Panel searchPanel = new Panel();
-  private VerticalLayout searchLayout = new VerticalLayout();
+  private HorizontalLayout mainLayout;
+  private VerticalLayout chartLayout;
+  private Panel searchPanel;
+  private VerticalLayout searchLayout;
   private OptionGroup categoryCombo;
   private OptionGroup userCombo;
   
@@ -53,6 +55,12 @@ public class ChartSheetView extends CustomComponent {
 
     if (expenseSheet.getFilter() == null)
       expenseSheet.setFilter(new Filter(null, null, null, null));
+    showCharts();
+    
+  }
+  
+  private void showCharts() {
+    chartLayout.removeAllComponents();
     List<YearCategory> yearCategoryList = ExpenseSheetService.prepareYearCategoryList(expenseSheet);
     
     Axis xAxis = new Axis(Axis.AxisType.xAxis);
@@ -94,42 +102,56 @@ public class ChartSheetView extends CustomComponent {
        HighChart columnChart = HighChartFactory.renderChart(columnConfiguration);
        columnChart.setSizeFull();
        
-       mainLayout.addComponent(lineChart);
-       mainLayout.addComponent(columnChart);
+       chartLayout.addComponent(lineChart);
+       chartLayout.addComponent(columnChart);
     } catch (HighChartsException e) {
        e.printStackTrace();
     }
   }
 
-  private VerticalLayout buildMainLayout() {
+  private HorizontalLayout buildMainLayout() {
     // common part: create layout
-    mainLayout = new VerticalLayout();
+    mainLayout = new HorizontalLayout();
     mainLayout.setSizeFull();
-    mainLayout.addComponent(buildSearchLayout());
+    Component chart = buildChartLayout();
+    mainLayout.addComponent(chart);
+    mainLayout.setExpandRatio(chart, 1);
+    Component search = buildSearchLayout();
+    mainLayout.addComponent(search);
+//    mainLayout.setExpandRatio(search, 1);
     // top-level component properties
-    setWidth("100.0%");
-    setHeight("200.0%");
+    setWidth("100%");
+    setHeight("200%");
     
     return mainLayout;
   }
   
+  private Component buildChartLayout() {
+    chartLayout = new VerticalLayout();
+    chartLayout.setHeight("100%");
+    chartLayout.setWidth("85%");
+    return chartLayout;
+  }
+  
   private Component buildSearchLayout() {
-    searchPanel.setVisible(false);
-    searchPanel.setContent(searchLayout);
-    searchLayout.removeAllComponents();
+    searchLayout = new VerticalLayout();
+    searchLayout.setSizeUndefined();
     searchLayout.setMargin(true);
     searchLayout.setSpacing(true);
+    
+    searchPanel = new Panel();
+//    searchLayout.setVisible(false);
+    searchPanel.setSizeUndefined();
+    searchPanel.setContent(searchLayout);
 
     // categoryCombo
     categoryCombo = new OptionGroup(Msg.get("expense.category"));
-    categoryCombo.setStyleName("v-select-optiongroup-horizontal");
     categoryCombo.setMultiSelect(true);
     categoryCombo.addItems(expenseSheet.getCategoryList());
     searchLayout.addComponent(categoryCombo);
 
     // userCombo
     userCombo = new OptionGroup(Msg.get("expense.user"));
-    userCombo.setStyleName("v-select-optiongroup-horizontal");
     userCombo.setMultiSelect(true);
     userCombo.addItems(expenseSheet.getUserLimitList());
     searchLayout.addComponent(userCombo);
@@ -140,24 +162,29 @@ public class ChartSheetView extends CustomComponent {
     searchButton.addClickListener(new Button.ClickListener() {
       private static final long serialVersionUID = 1L;
 
+      @SuppressWarnings("unchecked")
       @Override
       public void buttonClick(ClickEvent event) {
         User filterUser = null;
         String filterFormula = null;
         String filterComment = null;
-        if (userCombo.getValue() instanceof UserLimit) {
-          filterUser = ((UserLimit) userCombo.getValue()).getUser();
-        }
-        List<Category> categories = new ArrayList<Category>();
-        categories.add((Category) categoryCombo.getValue());
         List<User> users = new ArrayList<User>();
+        Set<UserLimit> setUserLimit = (Set<UserLimit>) userCombo.getValue();
+        for(Iterator<UserLimit> iter = setUserLimit.iterator(); iter.hasNext();)
+          users.add(iter.next().getUser());
         users.add((User) filterUser);
+        
+        List<Category> categories = new ArrayList<Category>();
+        Set<Category> setCategory = (Set<Category>) categoryCombo.getValue();
+        for(Iterator<Category> iter = setCategory.iterator(); iter.hasNext();)
+          categories.add(iter.next());
+        
         expenseSheet.setFilter(new Filter(categories, users, filterFormula, filterComment));
+        showCharts();
       }
     });
 
     searchLayout.addComponent(searchButton);
-    searchLayout.setComponentAlignment(searchButton, Alignment.BOTTOM_RIGHT);
 
     return searchPanel;
   }
