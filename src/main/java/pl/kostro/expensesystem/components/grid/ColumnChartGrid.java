ecad45ddd67e677b857f40eaccf256d7ac675982
Michał Kostro <michal.kostro@cgi.com>
2017-06-14 10:27:46 +0200

    zmiana sposobu przekazania wartości do row

--- src/main/java/pl/kostro/expensesystem/components/grid/ColumnChartGrid.java
+++ src/main/java/pl/kostro/expensesystem/components/grid/ColumnChartGrid.java
@@ -18,10 +18,19 @@ public class ColumnChartGrid extends Grid {
       columnList.add(monthName);
     setColumns(columnList);
     List<String> rowList = new ArrayList<>();
-    for (YearCategory yearCategory : yearCategoryList) {
-      rowList.add(yearCategory.getYear()+"");
-      rowList.addAll(yearCategory.getMonths());
-      addRow(rowList);
-    }
+    for (YearCategory yearCategory : yearCategoryList)
+      addRow(yearCategory.getYear()+"",
+          yearCategory.getMonthValue(0),
+          yearCategory.getMonthValue(1),
+          yearCategory.getMonthValue(2),
+          yearCategory.getMonthValue(3),
+          yearCategory.getMonthValue(4),
+          yearCategory.getMonthValue(5),
+          yearCategory.getMonthValue(6),
+          yearCategory.getMonthValue(7),
+          yearCategory.getMonthValue(8),
+          yearCategory.getMonthValue(9),
+          yearCategory.getMonthValue(10),
+          yearCategory.getMonthValue(11));
   }
 }
