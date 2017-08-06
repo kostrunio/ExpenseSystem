bfad9aac3615bee8598d9eefff7bcb40b2c59be3
Michal Kostro <miketo@o2.pl>
2017-08-06 22:25:20 +0200

    version with SpringBoot

--- src/main/java/pl/kostro/expensesystem/SpringMain.java
+++ src/main/java/pl/kostro/expensesystem/SpringMain.java
@@ -1,5 +1,6 @@
 package pl.kostro.expensesystem;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -12,6 +13,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan
 @EnableScheduling
 public class SpringMain extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMain.class, args);
+  }
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
