03f66f7d5cd26de59318509ca1cfe93fbf5d6429
Michal Kostro <michal.kostro@10g.pl>
2020-03-11 11:11:20 +0100

    update spring version

--- src/main/java/pl/kostro/expensesystem/SpringMain.java
+++ src/main/java/pl/kostro/expensesystem/SpringMain.java
@@ -1,26 +1,13 @@
 package pl.kostro.expensesystem;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-@EnableScheduling
-public class SpringMain extends SpringBootServletInitializer {
+@SpringBootApplication
+public class SpringMain {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringMain.class, args);
-  }
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(SpringMain.class);
   }
 
 }
