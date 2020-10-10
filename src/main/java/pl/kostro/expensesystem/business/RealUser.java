60683f127c6ed87ebf0064223bc68672e44eaf26
Michał Kostro <michal.kostro@10g.pl>
2020-10-10 22:48:21 +0200

    created business Classes

---
+++ src/main/java/pl/kostro/expensesystem/business/RealUser.java
@@ -0,0 +1,82 @@
+package pl.kostro.expensesystem.business;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RealUser extends User {
+  private String password;
+  private byte[] password_byte;
+  private String email;
+  private LocalDateTime logDate;
+  private List<ExpenseSheet> expenseSheetList;
+  private ExpenseSheet defaultExpenseSheet;
+  String clearPassword;
+
+  public RealUser(String name) {
+    super(name);
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public byte[] getPasswordByte() {
+    return password_byte;
+  }
+
+  public void setPasswordByte(byte[] password_byte) {
+    this.password_byte = password_byte;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public LocalDateTime getLogDate() {
+    return logDate;
+  }
+
+  public void setLogDate(LocalDateTime logDate) {
+    this.logDate = logDate;
+  }
+
+  public List<ExpenseSheet> getExpenseSheetList() {
+    if (expenseSheetList == null)
+      expenseSheetList = new ArrayList<ExpenseSheet>();
+    return expenseSheetList;
+  }
+
+  public void setExpenseSheetList(List<ExpenseSheet> expenseSheetList) {
+    this.expenseSheetList = expenseSheetList;
+  }
+
+  public ExpenseSheet getDefaultExpenseSheet() {
+    return defaultExpenseSheet;
+  }
+
+  public void setDefaultExpenseSheet(ExpenseSheet defaultExpenseSheet) {
+    this.defaultExpenseSheet = defaultExpenseSheet;
+  }
+
+  public String getClearPassword() {
+    return clearPassword;
+  }
+
+  public void setClearPassword(String clearPassword) {
+    this.clearPassword = clearPassword;
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName()+"["+getName()+"]";
+  }
+}
---
+++ src/main/java/pl/kostro/expensesystem/business/User.java
@@ -0,0 +1,43 @@
+package pl.kostro.expensesystem.business;
+
+import java.time.LocalDateTime;
+
+public class User {
+  private Long id;
+  private String name;
+  private LocalDateTime creationDate = LocalDateTime.now();
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDateTime getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(LocalDateTime creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName()+"["+getName()+"]";
+  }
+
+}
