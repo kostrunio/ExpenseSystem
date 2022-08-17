fac84e69264e22c2924a0d400ac9124f238a882d
Michał Kostro <michal.kostro@10g.pl>
2022-08-17 12:36:17 +0200

    show two lines of notification

--- src/main/java/pl/kostro/expensesystem/newui/notification/ShowNotification.java
+++ src/main/java/pl/kostro/expensesystem/newui/notification/ShowNotification.java
@@ -1,9 +1,12 @@
 package pl.kostro.expensesystem.newui.notification;
 
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import pl.kostro.expensesystem.utils.msg.Msg;
 
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 
 public class ShowNotification {
   
@@ -29,80 +32,75 @@ public class ShowNotification {
   }
   
   public static void registerOK() {
-    Notification notification = new Notification(Msg.get("notification.registerOK"));
-//    notification.setDescription(Msg.get("notification.registerUserOK"));
+    Notification notification = prepareNotification(Msg.get("notification.registerOK"), Msg.get("notification.registerUserOK"));
     setSuccessStyle(notification);
   }
   
   public static void registerProblem(String user) {
-    Notification notification = new Notification(Msg.get("notification.registerUserProblem"));
-//    notification.setDescription(MessageFormat.format(Msg.get("notification.registerUserDesc"), user));
+    Notification notification = prepareNotification(Msg.get("notification.registerUserProblem"), MessageFormat.format(Msg.get("notification.registerUserDesc"), user));
     setErrorStyle(notification);
   }
   
   public static void registerProblem() {
-    Notification notification = new Notification(Msg.get("notification.registerProblem"));
-//    notification.setDescription(Msg.get("notification.registerDesc"));
+    Notification notification = prepareNotification(Msg.get("notification.registerProblem"), Msg.get("notification.registerDesc"));
     setErrorStyle(notification);
   }
 
   public static void logonProblem() {
-    Notification notification = new Notification(Msg.get("notification.logonProblem"));
-//    notification.setDescription(Msg.get("notification.logonDesc"));
+    Notification notification = prepareNotification(Msg.get("notification.logonProblem"), Msg.get("notification.logonDesc"));
     setErrorStyle(notification);
   }
   
   public static void dbProblem(String msg) {
-    Notification notification = new Notification(Msg.get("notification.dbProblem"));
-//    notification.setDescription((MessageFormat.format(Msg.get("notification.dbDesc"), msg)));
+    Notification notification = prepareNotification(Msg.get("notification.dbProblem"), MessageFormat.format(Msg.get("notification.dbDesc"), msg));
     setErrorStyle(notification);
   }
   
   public static void changeSummary(String userName, BigDecimal actSum, BigDecimal newSum) {
-    Notification notification = new Notification(Msg.get("notification.sum"));
-//    notification.setDescription(MessageFormat.format(Msg.get("notification.sumDesc"), new Object[] {userName, actSum, newSum}));
+    Notification notification = prepareNotification(Msg.get("notification.sum"), MessageFormat.format(Msg.get("notification.sumDesc"), new Object[] {userName, actSum, newSum}));
     setWarnStyle(notification);
   }
 
   public static void fieldEmpty(String fieldName) {
-    Notification notification = new Notification(Msg.get("notification.formProblem"));
-//    notification.setDescription(MessageFormat.format(Msg.get("notification.formDesc"), fieldName));
+    Notification notification = prepareNotification(Msg.get("notification.formProblem"), MessageFormat.format(Msg.get("notification.formDesc"), fieldName));
     setErrorStyle(notification);
   }
 
   public static void noSuchUser(String name) {
-    Notification notification = new Notification(Msg.get("notification.noSuchUserProblem"));
-//    notification.setDescription(MessageFormat.format(Msg.get("notification.noSuchUserDesc"), name));
+    Notification notification = prepareNotification(Msg.get("notification.noSuchUserProblem"), MessageFormat.format(Msg.get("notification.noSuchUserDesc"), name));
     setErrorStyle(notification);
   }
 
   public static void removeOwnerProblem() {
-    Notification notification = new Notification(Msg.get("notification.removeOwnerProblem"));
-//    notification.setDescription(Msg.get("notification.removeOwnerDesc"));
+    Notification notification = prepareNotification(Msg.get("notification.removeOwnerProblem"), Msg.get("notification.removeOwnerDesc"));
     setErrorStyle(notification);
   }
 
   public static void badSheetPassword() {
-    Notification notification = new Notification(Msg.get("notification.badSheetPasswordProblem"));
-//    notification.setDescription(Msg.get("notification.badSheetPasswordDesc"));
+    Notification notification = prepareNotification(Msg.get("notification.badSheetPasswordProblem"), Msg.get("notification.badSheetPasswordDesc"));
     setErrorStyle(notification);
   }
 
   public static void passwordProblem() {
-    Notification notification = new Notification(Msg.get("notification.newSheetProblem"));
-//    notification.setDescription(Msg.get("notification.newSheetDesc"));
+    Notification notification = prepareNotification(Msg.get("notification.newSheetProblem"), Msg.get("notification.newSheetDesc"));
     setErrorStyle(notification);
   }
 
   public static void changeEmailOK() {
-    Notification notification = new Notification(Msg.get("notification.changeOK"));
-//    notification.setDescription(Msg.get("notification.emailChangeOK"));
+    Notification notification = prepareNotification(Msg.get("notification.changeOK"), Msg.get("notification.emailChangeOK"));
     setSuccessStyle(notification);
   }
 
   public static void changePasswordOK() {
-    Notification notification = new Notification(Msg.get("notification.changeOK"));
-//    notification.setDescription(Msg.get("notification.passwordChangeOK"));
+    Notification notification = prepareNotification(Msg.get("notification.changeOK"), Msg.get("notification.passwordChangeOK"));
     setSuccessStyle(notification);
+  }
+
+  private static Notification prepareNotification(String title, String description) {
+    VerticalLayout layout = new VerticalLayout();
+    Span titleLine = new Span(title);
+    Span descLine = new Span(description);
+    layout.add(titleLine, descLine);
+    return new Notification(layout);
   }
 }
